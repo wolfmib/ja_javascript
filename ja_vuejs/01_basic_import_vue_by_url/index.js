f57ce63064727976b5054918dbbd95589ca7e41f
Johnny_Wick <wolfmib@gmail.com>
2020-02-10 18:26:08 +0800

    save

--- ja_vuejs/01_basic_import_vue_by_url/index.js
+++ ja_vuejs/01_basic_import_vue_by_url/index.js
@@ -32,3 +32,16 @@ var var_dom_et_for_04 = new Vue({
         ]
     }
 })
+
+// Hadling User Input et Method
+var var_app_v_on = new Vue({
+    el: '#app-5',
+    data:{ 
+        message: " Je suis Johnny"
+    },
+    methods:{
+        reverse_message_func: function() {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
