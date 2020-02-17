f8733c6553ea928c11fae5c4fbe3ef423a611d75
Johnny_Wick <wolfmib@gmail.com>
2020-02-17 18:26:40 +0800

    in_progress_slot_machine_avec_python_grpc

---
+++ ja_vuejs/04_Slot_Machine_avec_Python_GRPC/server.py
@@ -0,0 +1,44 @@
+from concurrent import futures
+import grpc
+import gameslot_pb2
+import gameslot_pb2_grpc
+import time
+import threading 
+
+
+# Key: __grpc -> __Servicer
+class Listener(gameslot_pb2_grpc.GameSlotServiceServicer):
+    def __init__(self,*args,**kwargs):
+        self.counter = 0
+        self.lastPrintTime = time.time()
+
+    # the function from proto file's api_method_name
+        # rpc obtenir_slot_object (Slot_Object_Ping) returns (Slot_Object_Pong){}
+    def obtenir_slot_object(self, request,context):
+        self.counter += 1
+        
+        print("Got you")
+
+        return gameslot_pb2.Slot_Object_Pong(run_id=request.run_id + 1)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    # Key: __grpc -> Add___Servicer_to_server
+    gameslot_pb2_grpc.add_GameSlotServiceServicer_to_server(Listener(),server)
+
+
+    # Setting port
+    server.add_insecure_port("[::]:9999")
+    server.start()
+    try:
+        while True:
+            print("Server on: threads %i"%(threading.active_count()))
+            time.sleep(10)
+    except KeyboardInterrupt:
+        print("KeyboardInterrupt")
+        server.stop(0)
+
+
+if __name__ == "__main__":
+    serve()
--- ja_vuejs/Readme.md
+++ ja_vuejs/Readme.md
@@ -4,3 +4,20 @@
 SLOT-Machine for Example:
 https://github.com/guahsu/Vue-SlotMachine
 
+
+
+--- 
+- 01_basic_import_vue_by_url
+- 02_component_intro
+- 03_Slot_Machine
+    - go live by index.html 
+        - The code , explain the rolling logic with the json_configure_file in index.js
+    - go live by 5x3_full_index.html
+        - The code, explain the rolling logic with external 5x3_table.json, files list as following:
+            - index_5x3.html
+            - 5x3_table.json
+            - 5x3_index.js
+
+- 04_Slot_Machine_avec_Python_GRPC
+    - The example for FE Vue to get the rolling format form BE-GRPC Server (coding by Python)
+
