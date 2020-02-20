4dec4ad9b1034e8aeb0d37f2ff0f8cb8bea1ee8a
Johnny_Wick <wolfmib@gmail.com>
2020-02-20 16:01:03 +0800

    save

--- ja_vuejs/04_Slot_Machine_avec_Python_GRPC/index_5x3.js
+++ ja_vuejs/04_Slot_Machine_avec_Python_GRPC/index_5x3.js
@@ -6,12 +6,25 @@ const next = window.requestAnimationFrame ||
     function (cb) { window.setTimeout(cb, 1000 / 60) }
 
 
+// GRPC Setting Test
+
+//<script> const { Slot_object_Ping, Slot_Object_Pong } = require('./compiled_js_proto/proto/gameslot_pb.js')</script>
+//<script> const { GameSlotService } = require('./compiled_js_proto/proto/gameslot_grpc_web_pb.js') </script>
+
+const { Slot_object_Ping, Slot_Object_Pong } = require('./compiled_js_proto/proto/gameslot_pb.js')
+
+var my_slot_service = new GameSlotService('http://localhost:9999');
+var request = new Slot_object_Ping();
+//var response        = new Slot_Object_Pong();
+
+request.setMessage(543);
+
+my_slot_service.obtenir_slot_object(request, {}, function (err, response) {
+    console.log("I get you.... the response is following")
+    console.log(response)
+});
 
 
-
-
-
-// Load Excel
 
 
 // Cree the data format:
---
+++ ja_vuejs/04_Slot_Machine_avec_Python_GRPC_01_Try_gRPC-Web/Readme.md
@@ -0,0 +1,7 @@
+## I am lazy..
+---
+
+just take a look at the tutorial link:
+---
+
+https://github.com/grpc/grpc-web/tree/master/net/grpc/gateway/examples/helloworld
